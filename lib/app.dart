2457d8bcdfda29f3aac3b7530a18b00703f33ff2
mj <onyangomaureen95@gmail.com>
2019-09-12 22:26:00 +0300

    place splashscreen class as body

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:myportfolio/splashscreen.dart';
 
-class App extends StatelessWidget{
-  Widget build(context){
-  return MaterialApp(
-    
-  );
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SplashScreen(),
+      ),
+    );
   }
 }
