cf6732b332bf1867f088b9982e5145dcd03475ce
mj <onyangomaureen95@gmail.com>
2019-09-17 00:12:43 +0300

    place app description

--- lib/etailpage.dart
+++ lib/etailpage.dart
@@ -1,7 +1,62 @@
 import 'package:flutter/material.dart';
 
-class Etailor extends StatelessWidget{
-  Widget build(contxet){
-    return Scaffold();
+class Etailor extends StatelessWidget {
+  Widget build(contxet) {
+    return Scaffold(
+        backgroundColor: Color(0xFFcaadde),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          title: Text(
+            'Etailor App',
+            style: TextStyle(
+              color: Color(0xFF333366),
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(200.0),
+                      bottomLeft: Radius.circular(200.0))),
+              child: GridTile(
+                child: Container(
+                    child: Image.asset(
+                  'assets/sewingtools.jpg',
+                  height: 100,
+                  width: 100,
+                )),
+                footer: Container(
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: Text(
+                        'e-Tailor App',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20.0),
+                      ),
+                      trailing: Icon(
+                        Icons.favorite,
+                        color: Colors.purple,
+                      ),
+                    )),
+              ),
+            ),
+           
+            Container(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'This is an app to help facilitate tailor'
+                '-client activities like sending measurement details,'
+                'sending desired clothe designs and fabric designs to the tailor.',
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          ],
+        ));
   }
 }
