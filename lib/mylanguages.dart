129e0b45dad8a56ec7056f54e924a88b0de79b7d
mj <onyangomaureen95@gmail.com>
2019-09-18 04:02:37 +0300

    place a listview of items

--- lib/mylanguages.dart
+++ lib/mylanguages.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
 
-class Mylanguages extends StatelessWidget{
-  Widget build(context){
-    return Scaffold();
+class Mylanguages extends StatelessWidget {
+  Widget build(context) {
+    return Scaffold(
+      backgroundColor: Colors.purple[100],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        title: Text('My Languages/Skills'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text('Languages'),
+            subtitle: Text("Dart,\n"
+            "Flutter,\n"
+            "Elixir,\n"
+            "Phoenix,\n"
+            "HTML/CSS,\n"
+            "C#."),
+          ),
+          ListTile(
+            title: Text('Skills'),
+            subtitle: Text("Git workflow,\n"
+            'Linux,\n'
+            "Firebase,\n"
+            "SQL Database,\n"
+            "Agile Methodologies-Scrum,\n"
+            "Analysis of Algorithms,\n"
+            "Leadership and Collaboration."),
+          )
+        ],
+      ),
+    );
   }
 }
