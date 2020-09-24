a8e6a379e8f78d1e90a219f04fa2ece8abdb1b72
JosephineAkello <josephineakello2030@gmail.com>
2020-09-24 03:57:12 +0300

    add sentry url

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,32 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:sentry/sentry.dart';
+import 'dart:async';
 
-void main() {
-  runApp(App());
+
+
+final sentry = SentryClient(dsn: "https://sentry.io/jakello/my-portfolio");
+
+void main() async {
+  runZonedGuarded(
+    () => runApp(MyApp()),
+    (error, stackTrace) async {
+      await sentry.captureException(
+        exception: error,
+        stackTrace: stackTrace,
+      );
+    },
+  );
+
+ 
+
+  FlutterError.onError = (details, {bool forceReport = false}) {
+  sentry.captureException(
+    exception: details.exception,
+    stackTrace: details.stack,
+  );
+};
+
+
 }
+
