5bcc54f9a9d48253ebd40c08b8fde0958fc98457
JosephineAkello <josephineakello2030@gmail.com>
2020-09-25 16:59:28 +0300

    modify sentry events

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,68 @@ import 'package:sentry/sentry.dart';
 import 'dart:async';
 
 
-
 final sentry = SentryClient(dsn: "https://sentry.io/jakello/my-portfolio");
 
-void main() async {
-  runZonedGuarded(
-    () => runApp(MyApp()),
-    (error, stackTrace) async {
-      await sentry.captureException(
-        exception: error,
-        stackTrace: stackTrace,
-      );
-    },
+bool get isInDebugMode {
+  bool inDebugMode = false;
+  assert(inDebugMode = true);
+  return inDebugMode;
+}
+
+Future<Null> _reportError(dynamic error, dynamic stackTrace) async {
+  print('Caught error: $error');
+
+  // Errors thrown in development mode are unlikely to be interesting. You can
+  // check if you are running in dev mode using an assertion and omit sending
+  // the report.
+  if (isInDebugMode) {
+    print(stackTrace);
+    print('In dev mode. Not sending report to Sentry.io.');
+    return;
+  }
+
+    print('Reporting to Sentry.io...');
+
+  final SentryResponse response = await sentry.captureException(
+    exception: error,
+    stackTrace: stackTrace,
   );
 
+  if (response.isSuccessful) {
+    print('Success! Event ID: ${response.eventId}');
+  } else {
+    print('Failed to report to Sentry.io: ${response.error}');
+  }
+}
+
+Future<Null> main() async {
+  // This captures errors reported by the Flutter framework.
+  FlutterError.onError = (FlutterErrorDetails details) async {
+    if (isInDebugMode) {
+      // In development mode simply print to console.
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      // In production mode report to the application zone to report to
+      // Sentry.
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
+  runZonedGuarded<Future<Null>>(() async {
+    runApp(new MyApp());
+  }, (error, stackTrace) async {
+    await _reportError(error, stackTrace);
+  });
+}
  
 
-  FlutterError.onError = (details, {bool forceReport = false}) {
-  sentry.captureException(
-    exception: details.exception,
-    stackTrace: details.stack,
-  );
-};
 
 
-}
+
+
+
+
+
+
+
 
