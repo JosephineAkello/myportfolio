54c4186abafbf92b3ee9997f4beef8cde3530454
JosephineAkello <josephineakello2030@gmail.com>
2022-03-15 23:50:40 +0300

    add new sentry configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,18 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
-import 'package:sentry/sentry.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'dart:async';
 import 'mysecretes.dart';
 
 
-final sentry = SentryClient(dsn: "{$mydsn}");
-
-bool get isInDebugMode {
-  bool inDebugMode = false;
-  assert(inDebugMode = true);
-  return inDebugMode;
+Future<void> main() async {
+  await SentryFlutter.init(
+    (options) => options.dsn = 
+    '$mydsn',
+    appRunner: () => runApp(MyApp()));
 }
 
-Future<Null> _reportError(dynamic error, dynamic stackTrace) async {
-  print('Caught error: $error');
 
-  // Errors thrown in development mode are unlikely to be interesting. You can
-  // check if you are running in dev mode using an assertion and omit sending
-  // the report.
-  if (isInDebugMode) {
-    print(stackTrace);
-    print('In dev mode. Not sending report to Sentry.io.');
-    return;
-  }
-
-    print('Reporting to Sentry.io...');
-
-  final SentryResponse response = await sentry.captureException(
-    exception: error,
-    stackTrace: stackTrace,
-  );
-
-  if (response.isSuccessful) {
-    print('Success! Event ID: ${response.eventId}');
-  } else {
-    print('Failed to report to Sentry.io: ${response.error}');
-  }
-}
-
-Future<Null> main() async {
-  // This captures errors reported by the Flutter framework.
-  FlutterError.onError = (FlutterErrorDetails details) async {
-    if (isInDebugMode) {
-      // In development mode simply print to console.
-      FlutterError.dumpErrorToConsole(details);
-    } else {
-      // In production mode report to the application zone to report to
-      // Sentry.
-      Zone.current.handleUncaughtError(details.exception, details.stack);
-    }
-  };
-
-  runZonedGuarded<Future<Null>>(() async {
-    runApp(new MyApp());
-  }, (error, stackTrace) async {
-    await _reportError(error, stackTrace);
-  });
-}
  
 
 
