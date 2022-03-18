3fda7c7f340c95f0a61e5d9db3eeb9fbf74eda16
JosephineAkello <josephineakello2030@gmail.com>
2022-03-18 23:39:52 +0300

    create apptest file

---
+++ test_driver/app_test.dart
@@ -0,0 +1,22 @@
+import 'dart:async';
+import 'package:flutter_gherkin/flutter_gherkin.dart';
+import 'package:gherkin/gherkin.dart';
+import 'steps/home_screen_steps.dart';
+
+Future<void> main() {
+  final config = FlutterTestConfiguration()
+    ..features = [RegExp('features/*.*.feature')]
+    ..reporters = [
+      ProgressReporter(),
+      TestRunSummaryReporter()
+    ] // you can include the "StdoutReporter()" without
+    //the message level parameter for verbose log information
+    ..stepDefinitions = [
+      launchHomeScreenStep(),
+      afterLaunchHomeScreenStep()
+    ]
+    ..restartAppBetweenScenarios = true
+    ..targetAppPath = "test_driver/app.dart";
+  // ..tagExpression = "@smoke" // uncomment to see an example of running scenarios based on tag expressions
+  return GherkinRunner().execute(config);
+}
